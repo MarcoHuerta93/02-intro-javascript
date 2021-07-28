55e9d4c0affddc65e19ca47d43d48c681d402b55
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-28 02:45:19 -0500

    Template String

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 
 
-console.log('Marco');
+const nombre = 'Marco ';
+const apellido = 'Huerta';
+
+
+//const nombre completo = nombre + ' ' + apellido:
+
+const nombreCompleto = `
+${nombre} 
+${apellido}`;
+
+console.log(nombreCompleto);
+
+function getSaludo(nombre){
+    return 'Hola Saludo ' +  nombre;
+}
+
+console.log(`Este es un texto: ${ getSaludo(nombre)}`);
