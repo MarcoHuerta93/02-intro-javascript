9ed25f145fc308e1b1e6a693c0f673f730ae9365
Marco Huerta <marco.huerta.sc@pm.me>
2021-07-28 02:29:40 -0500

    Inicializando  fundamentos JS

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,23 @@
+
+
+// Variables y constantes 
+
+const nombre = 'Marco';
+
+const apellido = 'Huerta';
+
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+
+//var no se debe de usar 
+if (true) {
+
+
+    const nombre = 'Antonio';
+    console.log(nombre);
+    
+}
+
+console.log(valorDado);
